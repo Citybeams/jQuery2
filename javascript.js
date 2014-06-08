f08a0ed66606c58ae8560ec99da941c57c3df569
citybeams <nicklohr1@gmail.com>
2014-06-08 17:05:44 -0400

    slide down function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-  // this specifies which div to look in #tours, look for buttons "event delegation"
-  $('#tours').on('click', 'button', function(){
-    // returns the closest data price info when clicked
-    var vacation = $(this).closest('li');
-    //looks into the html for that price data added to the element
-    var amount = vacation.data('price');
-    var price = $('<p>From $'+amount+'</p>');
-    vacation.append(price);
-    $(this).remove();
+//   // this specifies which div to look in #tours, look for buttons "event delegation"
+//   $('#tours').on('click', 'button', function(){
+//     // returns the closest data price info when clicked
+//     var vacation = $(this).closest('li');
+//     //looks into the html for that price data added to the element
+//     var amount = vacation
+//     var price = $('<p>From $'+amount+'</p>');
+//     vacation.append(price);
+//     $(this).remove();
+//   });
+
+
+
+  $('.confirmation').on('click', 'button', function(){
+    $(this).closest('.confirmation').find('.ticket').slideDown();
   });
 });
-
