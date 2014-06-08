1bf6b8d5bd21f7ebc33fc0e8a2f34e4ba38a18d4
citybeams <nicklohr1@gmail.com>
2014-06-08 16:21:34 -0400

    event delegation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  alert("hi");
+  // this specifies which div to look in #tours, look for buttons "event delegation"
+  $('#tours').on('click', 'button', function(){
+    // returns the closest data price info when clicked
+    var vacation = $(this).closest('li');
+    //looks into the html for that price data added to the element
+    var amount = vacation.data('price');
+    var price = $('<p>From $'+amount+'</p>');
+    vacation.append(price);
+    $(this).remove();
+  });
 });
+
